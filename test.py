02f251b848ab1c97c1e4605ffb9e080320c5de25
tangchaoqun <tangchaoq@gmail.com>
2019-06-17 09:41:35 +0200

    testpy

--- test.py
+++ test.py
@@ -1,8 +1,19 @@
 # Cancella tutto per favore!
-rain = input("Dimmi tutto:")
+x=0
+while True:
+	mode=input("Dimmi tutto!: ")
+	if mode=="q":
+		break
+	elif mode=="1":
+		print("GIUSTO!")
+	elif mode=="2":
+		print("AVANTI,AMICI!")
+	else:
+		print("CHE CAZZO FAI! GUARDA SOLO 1/2/q")
+	x=x+1
+print("Sono fuori, dove sei?",x)
 
-f=int(rain)*9/5+20
-print('Sei',int(f))
+
 
 
 
