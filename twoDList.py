73b8ae0a0e714597487e9c9ab89f649b56ea90e0
tangchaoqun <tangchaoq@gmail.com>
2019-06-18 10:35:58 +0200

    2DList3

--- twoDList.py
+++ twoDList.py
@@ -10,3 +10,12 @@ while True:
 	# p=[name,price]
 	products.append([name,price])
 print(products)
+
+for p in products:
+	print(p[0],"'s price is",p[1])
+
+with open ('products.csv','w',encoding='utf-8') as f:
+	f.write('商品,价格\n')
+	for p in products:
+		f.write(p[0]+','+p[1]+'\n')
+
