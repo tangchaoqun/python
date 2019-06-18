accb487dde96cec91cad81959500c521c39cd204
tangchaoqun <tangchaoq@gmail.com>
2019-06-18 10:55:28 +0200

    2DList4

--- twoDList.py
+++ twoDList.py
@@ -1,4 +1,14 @@
+# Read CSV
 products=[]
+with open ('products.csv','r',encoding='utf-8') as f:
+	for line in f:
+		if '商品,价格' in line:
+			continue # Salta
+		name,price=line.strip().split(',')
+		products.append([name,price])
+print(products)
+
+# Input
 while True:
 	name=input("Please insert product's name:")
 	if name=="q":
@@ -11,6 +21,7 @@ while True:
 	products.append([name,price])
 print(products)
 
+#Output
 for p in products:
 	print(p[0],"'s price is",p[1])
 
